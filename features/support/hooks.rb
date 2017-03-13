94e0773033d129e346d4b3fa9d7421e752489127
abshira mohamed <amohamed@spartaglobal.co>
2017-03-13 13:55:22 +0000

    added hooks

---
+++ features/support/hooks.rb
@@ -0,0 +1,16 @@
+# Hooks File
+
+browser = Watir::Browser.new :chrome
+
+Before do
+  @b = browser
+  @b.cookies.clear
+end
+
+After do |scenario|
+  # Execute after every scenario
+end
+
+at_exit do
+  browser.quit
+end
